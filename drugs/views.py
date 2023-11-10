9bee9ef7b6a4a404533feb318e20e96cd6aaa3d6
Ibrokhimjon0823 <ibrokhimjonmakhamadaliev@gmail.com>
2023-11-10 18:06:58 +0100

    added views

--- drugs/views.py
+++ drugs/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import generics
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Drug, Disease
+from .serializers import DrugDiseaseSearchSerializer
+from django_filters import FilterSet, CharFilter
 
-# Create your views here.
+
+class DrugDiseaseFilter(FilterSet):
+    query = CharFilter(method='filter_query')
+
+    class Meta:
+        model = Drug
+        fields = []
+
+    def filter_query(self, queryset, name, value):
+        return queryset.filter(
+            Drug.Q(name__icontains=value) |  # Search drug name
+            Disease.Q(diseases__name__icontains=value)  # Search disease name
+        ).distinct()
+
+
+class DrugDiseaseSearchView(generics.ListAPIView):
+    serializer_class = DrugDiseaseSearchSerializer
+    queryset = Drug.objects.all()
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = DrugDiseaseFilter
