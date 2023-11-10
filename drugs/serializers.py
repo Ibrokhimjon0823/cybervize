8ad6ad9c7c136edf507720d25196720b8fedb02d
Ibrokhimjon0823 <ibrokhimjonmakhamadaliev@gmail.com>
2023-11-10 22:29:05 +0100

    changed serializers.py

--- drugs/serializers.py
+++ drugs/serializers.py
@@ -1,5 +1,23 @@
 from rest_framework import serializers
-
+from drugs.models import Drug, Disease
 
 class DrugDiseaseSearchSerializer(serializers.Serializer):
     query = serializers.CharField()
+
+
+class DiseaseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Disease
+        fields = '__all__'
+
+
+class DrugSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Drug
+        fields = '__all__'
+
+
+class SearchResultSerializer(serializers.Serializer):
+    drugs = DrugSerializer(many=True)
+    diseases = DiseaseSerializer(many=True)
+
