f4696261ffbbb7566bee2c42c1f265e120754a16
Ibrokhimjon0823 <ibrokhimjonmakhamadaliev@gmail.com>
2023-11-10 17:58:15 +0100

    added models

--- drugs/models.py
+++ drugs/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Drug(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False, unique=True)
+    diseases = models.ManyToManyField('Disease')
+    description = models.TextField()
+    name = models.CharField(max_length=255)
+    released = models.DateField()
+
+    def formatted_release_date(self):
+        return self.released.strftime('%d/%m/%Y')
+
+    def __str__(self):
+        return self.name
+
+
+class Disease(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
