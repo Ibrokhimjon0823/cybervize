ed70161828bbf2f7eef4eab400c3d60979bcb5f2
Ibrokhimjon0823 <ibrokhimjonmakhamadaliev@gmail.com>
2023-11-11 22:06:42 +0100

    changed serializers.py

--- drugs/serializers.py
+++ drugs/serializers.py
@@ -8,16 +8,21 @@ class DrugDiseaseSearchSerializer(serializers.Serializer):
 class DiseaseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Disease
-        fields = '__all__'
+        fields = ('id', 'name')
 
 
 class DrugSerializer(serializers.ModelSerializer):
+    formatted_release_date = serializers.SerializerMethodField()
+    diseases = DiseaseSerializer(many=True, read_only=True)
     class Meta:
         model = Drug
-        fields = '__all__'
+        fields = ('id', 'diseases', 'description', 'name', 'formatted_release_date')
+
+    def get_formatted_release_date(self, obj):
+        return obj.formatted_release_date()
 
 
 class SearchResultSerializer(serializers.Serializer):
     drugs = DrugSerializer(many=True)
-    diseases = DiseaseSerializer(many=True)
+    diseases = DiseaseSerializer(many=True, read_only=True)
 
