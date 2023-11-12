118b9ec469405c9d6042b0fe2cdf52e5b282c749
Ibrokhimjon0823 <ibrokhimjonmakhamadaliev@gmail.com>
2023-11-12 21:02:26 +0100

    refactored fixtures.py

--- fixtures/fixtures.py
+++ fixtures/fixtures.py
@@ -4,45 +4,45 @@ import os
 import django
 import pathlib
 
-
-path = pathlib.Path(__file__).parent.resolve()
-
-
-sys.path.append(str(path.parent))
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
-
-# Initialize Django ORM
-django.setup()
-
 from drugs.models import Drug, Disease
 
-# Load JSON data from the file
-json_file = path/'dataset.json'
 
-try:
-    with open(json_file, 'r') as file:
-        data = json.load(file)
+def load_data():
+    pass
 
-    drugs_data = data.get('drugs', [])  # Extract 'drugs' data from the JSON
+    path = pathlib.Path(__file__).parent.resolve()
 
-    for drug_data in drugs_data:
-        # Extract 'diseases' data and create Disease instances
-        diseases_data = drug_data.get('diseases', [])
-        diseases = [Disease.objects.get_or_create(name=disease)[0] for disease in diseases_data]
+    sys.path.append(str(path.parent))
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
 
-        # Create and save the Drug instance
-        drug = Drug(
-            id=drug_data['id'],
-            description=drug_data['description'],
-            name=drug_data['name'],
-            released=drug_data['released']
-        )
-        drug.save()
+    django.setup()
 
-        # Add the related diseases to the drug
-        drug.diseases.add(*diseases)
+    json_file = path/'dataset.json'
 
-    print(f"Successfully loaded {len(drugs_data)} drugs into the database.")
+    try:
+        with open(json_file, 'r') as file:
+            data = json.load(file)
 
-except Exception as e:
-    print(f"An error occurred: {str(e)}")
+        drugs_data = data.get('drugs', [])
+
+        for drug_data in drugs_data:
+            diseases_data = drug_data.get('diseases', [])
+            diseases = [Disease.objects.get_or_create(name=disease)[0] for disease in diseases_data]
+            drug = Drug(
+                id=drug_data['id'],
+                description=drug_data['description'],
+                name=drug_data['name'],
+                released=drug_data['released']
+            )
+            drug.save()
+
+            drug.diseases.add(*diseases)
+
+        print(f"Successfully loaded {len(drugs_data)} drugs into the database.")
+
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
+
+if __name__ == "__main__":
+    load_data()
